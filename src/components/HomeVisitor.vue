92e1911e48654aa40312d1716c86248c112dddee
Lucas Ritter <lucasjritter@gmail.com>
2023-10-25 13:44:19 -0300

    subindo
    111

--- src/components/HomeVisitor.vue
+++ src/components/HomeVisitor.vue
@@ -2,7 +2,7 @@
     <v-container>
       <v-row class="text-center">
         <v-col class="mb-4">
-          <h1 class="display-2 font-weight-bold mb-3">
+          <h1 class="mb-42">
             Bem-vindo ao E-Sportify
           </h1>
   
@@ -13,7 +13,7 @@
         </v-col>
         
         <v-col class="mb-5" cols="12">
-          <h2 class="headline font-weight-bold mb-3">
+          <h2 class="mb-52">
             Acesse funcionalidades avançadas
           </h2>
   
@@ -22,38 +22,62 @@
           </v-row>
         </v-col>
   
-        <!-- <v-col cols="12">
+        <v-col cols="12">
           <v-img
-            :src="require('../assets/logo.svg')"
+            :src="require('../assets/logo.png')"
             class="my-3"
             contain
             height="200"
           />
-        </v-col> -->
+        </v-col>
 
       </v-row>
     </v-container>
   </template>
   
-  <script>
-  export default {
-    name: 'HomeVisitor',
-    methods: {  methods: {
-    login() {
-      // Redirecionar para a página de login
-      this.$router.push('/login');
-    },
-    register() {
-      // Redirecionar para a página de registro
-      this.$router.push('/registrar');
-    },
-  }
-}
-}
-  </script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Quantico&display=swap');
+
 .btn-registrar{
   text-decoration: none;
 }
 
+*, mb-42, mb-52 {
+  font-family: 'Quantico', sans-serif;
+  color: white;
+}
+
+body {
+  background-image: url('/src/assets/bg.png'); 
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+.mb-4{
+  margin-left: 45rem;
+  margin-top: 12rem;
+  font-size: 1.3rem;
+}
+
 </style>
+
+
+
+<script>
+export default {
+  name: 'HomeVisitor',
+  methods: {  methods: {
+  login() {
+    // Redirecionar para a página de login
+    this.$router.push('/login');
+  },
+  register() {
+    // Redirecionar para a página de registro
+    this.$router.push('/registrar');
+  },
+}
+}
+}
+</script>
