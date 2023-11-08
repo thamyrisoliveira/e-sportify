2f49402a751ae8830b7a71da6770ef310569a8b7
Lucas Ritter <lucasjritter@gmail.com>
2023-11-08 12:28:28 -0300

    carrosel
    ^

--- src/components/HomeAuth.vue
+++ src/components/HomeAuth.vue
@@ -1,19 +1,30 @@
 <template>
- <v-container>
- </v-container>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, index) in items"
+      :key="index"
+    >
+      <v-sheet
+        class="text-center"
+        :color="item.color"
+      >
+        <v-img
+          :src="item.image"
+          max-width="200"
+          max-height="150"
+          contain
+        ></v-img>
+        <div class="caption">
+          {{ item.caption }}
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
-
-<style>
-body {
-  overflow: hidden;
-}
-
-
-</style>
 
 <script>
 export default {
-  name: 'HomeAuth',
+  name: 'HomeAuth', 
   methods: {
     login() {
       // Redirecionar para a página de login
@@ -23,7 +34,43 @@ export default {
       // Redirecionar para a página de registro
       this.$router.push('/registrar');
     },
-  }
-}
+      },
+  data() {
+    return {
+      items: [
+        {
+          image: 'URL_DA_IMAGEM_1',
+          caption: 'Imagem 1',
+          color: 'primary',
+        },
+        {
+          image: 'URL_DA_IMAGEM_2',
+          caption: 'Imagem 2',
+          color: 'secondary',
+        },
+        {
+          image: 'URL_DA_IMAGEM_3',
+          caption: 'Imagem 3',
+          color: 'info',
+        },
+        // Adicione mais itens semelhantes conforme necessário
+      ],
+    };
+  },
+};
+
+
+
+
 
 </script>
+
+<style>
+.caption {
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 10px;
+}
+</style>
+
