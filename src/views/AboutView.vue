10d7595c1b4c518028c73697c9e78f06c4f1f3d9
Lucas Ritter <51051129+lukebemis@users.noreply.github.com>
2023-11-11 13:16:53 -0300

    -
    -

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="about">
+    <div>
       <h1>Nosso objetivo é criar um espaço único e acessível onde jogadores profissionais de PUBG possam exibir suas habilidades, encontrar oportunidades de crescimento e se conectar com organizações de esports dispostas a investir no seu potencial. Queremos ser a plataforma definitiva que molda o futuro dos esports no universo do PUBG.</h1>
       <h1> Conecte-se com os melhores jogadores e organizações de esports.</h1>
     </div>
